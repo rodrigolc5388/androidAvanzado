17a9cd4494e90702d52a1dc0bb5b512692ed5de5
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-02-04 18:41:25 +0100

    App prepared for deleteAllShops from DDBB: cache, repository and deleteAllShopsInteractor

--- app/src/main/java/com/rodrigolc/madridshops/MadridShopsApp.kt
+++ app/src/main/java/com/rodrigolc/madridshops/MadridShopsApp.kt
@@ -1,48 +1,5 @@
 package com.rodrigolc.madridshops
 
-import android.support.multidex.MultiDexApplication
-import android.util.Log
-import com.rodrigolc.madridshops.domain.Interactor.ErrorCompletion
-import com.rodrigolc.madridshops.domain.Interactor.getAllShops.GetAllShopsInteractorFakeImpl
-import com.rodrigolc.madridshops.domain.Interactor.SuccessCompletion
-import com.rodrigolc.madridshops.domain.Interactor.deleteallshops.DeleteAllShopsImpl
-import com.rodrigolc.madridshops.domain.model.Shops
-
-
-class MadridShopsApp: MultiDexApplication() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        // init code application wide
-
-        Log.d("App", "onCreate")
-
-        val allShopsInteractor = GetAllShopsInteractorFakeImpl()
-        allShopsInteractor.execute(
-                success = object: SuccessCompletion<Shops>{
-                    override fun successCompletion(shops: Shops) {
-                        Log.d("Shops", "Count: " + shops.count())
-
-                    }
-
-                },
-                error = object: ErrorCompletion {
-                    override fun errorCompletion(errorMessage: String) {
-                    }
-                }
-        )
-
-        DeleteAllShopsImpl(this).execute(success = {
-
-        }, error = {
-
-        })
-
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-    }
-
-}
+/**
+ * Created by Rodrigo on 1/2/18.
+ */
