829f178575fcb947ae448faf501eb399e88ef8b6
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-02-18 09:16:19 +0100

    Starting to work on DetailActivity

---
+++ app/src/main/java/com/rodrigolc/madridshops/activity/DetailActivity.kt
@@ -0,0 +1,3 @@
+package com.rodrigolc.madridshops.activity
+
+
---
+++ app/src/main/java/com/rodrigolc/madridshops/fragment/DetailFragment.kt
@@ -0,0 +1,25 @@
+package com.rodrigolc.madridshops.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.rodrigolc.madridshops.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}// Required empty public constructor
