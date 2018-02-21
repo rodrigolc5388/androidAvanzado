2d293d3e2dc3f509f7e3516e31a8e90ec3444683
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-02-21 16:04:53 +0100

    InternetStatus check working on HomeActvity; some improvements to do

--- app/src/main/java/com/rodrigolc/madridshops/activity/HomeActivity.kt
+++ app/src/main/java/com/rodrigolc/madridshops/activity/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.rodrigolc.madridshops.activity
 
+import android.app.AlertDialog
 import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -10,6 +11,7 @@ import com.rodrigolc.madridshops.domain.interactor.ErrorCompletion
 import com.rodrigolc.madridshops.domain.interactor.SuccessCompletion
 import com.rodrigolc.madridshops.domain.interactor.getAllShops.GetAllShoptivitiesInteractor
 import com.rodrigolc.madridshops.domain.interactor.getAllShopshops.GetAllShoptivitiesInteractorImplementation
+import com.rodrigolc.madridshops.domain.interactor.internetstatus.InternetStatusInteractorImpl
 import com.rodrigolc.madridshops.domain.model.Shoptivities
 import com.rodrigolc.madridshops.fragment.HomeFragment
 import com.rodrigolc.madridshops.router.Router
@@ -25,7 +27,7 @@ class HomeActivity: AppCompatActivity(), HomeFragment.OnSelectedSectionListener 
         setContentView(R.layout.activity_home)
         //setSupportActionBar(toolbar)
 
-        dataBaseTrigger()
+        checkInternetConnection()
 
         if (fragmentManager.findFragmentById(R.id.home_fragment) == null) {
             val fragment: Fragment = HomeFragment.newInstance()
@@ -47,6 +49,29 @@ class HomeActivity: AppCompatActivity(), HomeFragment.OnSelectedSectionListener 
             }
         })
     }
+
+    private fun checkInternetConnection(){
+        InternetStatusInteractorImpl().execute(this, success = {
+            dataBaseTrigger()
+        }, error = {
+            AlertDialog.Builder(this)
+                    .setTitle("No internet connection")
+                    .setMessage("There is no internet connection. Please solve the issue and try again.")
+                    .setPositiveButton("Ok", {dialog, _ ->
+                        dialog.dismiss()
+                        //dataBaseTrigger()
+                        this.finish()
+
+                    })
+                    /*.setNegativeButton("Retry",{ dialog, _ ->
+                        dialog.dismiss()
+                        checkInternetConnection()
+                    })*/
+                    .show()
+
+        })
+    }
+
 
 
     override fun onSelectedSection(section: String) = Router().navigateFromHomeActivityToMainActivity(this, section)
